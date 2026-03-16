fd33c75fe1d39658321704294b4108bfbff66824
Larissa Maiero <larissamaiero@gmail.com>
2026-03-16 09:20:59 -0300

    Add Variaveis class with example of string concatenation and integer parsing

---
+++ aula02/Variaveis.java
@@ -0,0 +1,14 @@
+package aula02;
+
+public class Variaveis {
+    public static void main(String[] args) {
+        String msgPt1 = "Olá";
+        String msgPt2 = "alunos";
+        System.out.println(msgPt1.concat(msgPt2));
+
+        int n1 = 1;
+        String n2 = "2";
+        System.out.println(n1 + n2);
+        System.out.println(n1 + Integer.parseInt(n2));
+    }
+}
