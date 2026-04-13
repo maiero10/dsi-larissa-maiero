9481f9c1ace394c2023116dfa6d7a805aa21e48a
Larissa Maiero <larissamaiero@gmail.com>
2026-04-13 08:20:22 -0300

    Add Variaveis class with basic arithmetic example

---
+++ aula03/Variaveis.java
@@ -0,0 +1,14 @@
+package aula03;
+
+//Camel Case = NomeVariavel - variáveis, atributos e métodos
+//Pastel Case = nomeVariavel - classes, interfaces e pacotes
+//Snake Case = nome_variavel - constantes
+//Kebab Case = nome-variavel - não é utilizado em Java
+public class Variaveis {
+    public static void main(String[] args) {
+        int n1, n2, n3;
+        n1 = n2 = 10;
+        n3 = 5;
+        System.out.println(n1 + n2 + n3);
+    }
+}
